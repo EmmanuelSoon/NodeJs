ae93cf610df190da64589c5dff4b96b43f7644c2
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-27 23:18:59 +0800

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1175264909873720',
+        clientSecret: 'cdd8f96929e8583e9f7765eb17857e94'
+    }
 }
