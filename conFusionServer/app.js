d0003be0931f18fe987680e12eb7d5fa07f40a80
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-27 23:33:08 +0800

    Assignment 4

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -17,6 +17,7 @@ const dishRouter = require('./routes/dishRouter');
 const leaderRouter = require('./routes/leaderRouter');
 const promoRouter = require('./routes/promoRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 
 const mongoose = require('mongoose');
@@ -89,6 +90,8 @@ app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
+
 
 
 // catch 404 and forward to error handler
